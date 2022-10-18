83c832a5ce4af94a1858fe48f0c7cef3a1d23d2f
g00401655 <g00401655@atu.ie>
2022-10-18 13:40:09 +0100

    prompt user for width

--- src/main/java/ie/atu/week5/ShapeApp.java
+++ src/main/java/ie/atu/week5/ShapeApp.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class ShapeApp {
     public static void main(String[] args) {
-        System.out.println("Please enter the Width of the rectangle");
+        System.out.println("Please enter the length of the rectangle");
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
         System.out.println("you entered : " + length);
+
+        System.out.println("Please enter the width of the rectangle");
+        double width = input.nextDouble();
+        System.out.println("you entered : " + width);
 
     }
 }
